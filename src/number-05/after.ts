efc1ebb97d3a74bd6bd7b8812bcb1573f29ee54b
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-29 12:01:18 +0330

    [ADD] number 05

--- src/number-05/after.ts
+++ src/number-05/after.ts
@@ -1,1 +1,15 @@
-// Code Smell: Name | After
+// Code Smell: Long Parameter List | After
+
+export function generateReport(
+  math: Course,
+  science: Course,
+  gym: Course,
+): void {
+  // ...
+}
+
+interface Course {
+  professor: string;
+  sessions: number;
+  grade: number;
+}
--- src/number-05/before.ts
+++ src/number-05/before.ts
@@ -1,1 +1,15 @@
-// Code Smell: Name | Before
+// Code Smell: Long Parameter List | Before
+
+export function generateReport(
+  mathProfessor: string,
+  mathSessions: number,
+  mathGrade: number,
+  scienceProfessor: string,
+  scienceSessions: number,
+  scienceGrade: number,
+  gymProfessor: string,
+  gymSessions: number,
+  gymGrade: number,
+): void {
+  // ...
+}
