ce38506427c79376ae0404276128f0bc0703516d
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-29 07:12:20 +0330

    [ADD] number 01

--- src/number-01/after.ts
+++ src/number-01/after.ts
@@ -1,1 +1,41 @@
-// Code Smell: Name | After
+// Code Smell: Long Method | After
+import { TaskModel } from "../demo-purpose/models/task.model";
+
+function appendTaskToList(task: TaskModel): void {
+  const listElement = document.querySelector("#tasks")!;
+
+  const taskElement = createTaskElement(task);
+
+  listElement.append(taskElement);
+}
+
+function createTaskElement(task: TaskModel): HTMLLIElement {
+  const taskElement = document.createElement("li");
+  taskElement.innerHTML = generateTaskElementInnerHTML(task);
+
+  addInputEventListeners(task, taskElement);
+
+  return taskElement;
+}
+
+function generateTaskElementInnerHTML(task: TaskModel): string {
+  return `
+    <li>
+      <label>
+        <input type="checkbox" checked="${task.isDone}" />
+        <div class="title">${task.title}</div>
+      </label>
+    </li>
+  `;
+}
+
+function addInputEventListeners(
+  task: TaskModel,
+  taskElement: HTMLLIElement,
+): void {
+  const inputElement = taskElement.querySelector("input")!;
+
+  inputElement.addEventListener("change", () => {
+    task.isDone = inputElement.checked;
+  });
+}
--- src/number-01/before.ts
+++ src/number-01/before.ts
@@ -1,1 +1,23 @@
-// Code Smell: Name | Before
+// Code Smell: Long Method | Before
+import { TaskModel } from "../demo-purpose/models/task.model";
+
+function appendTaskToList(task: TaskModel): void {
+  const listElement = document.querySelector("#tasks")!;
+
+  const taskElement = document.createElement("li");
+  taskElement.innerHTML = `
+    <li>
+      <label>
+        <input type="checkbox" checked="${task.isDone}" />
+        <div class="title">${task.title}</div>
+      </label>
+    </li>
+  `;
+
+  const inputElement = taskElement.querySelector("input")!;
+  inputElement.addEventListener("change", () => {
+    task.isDone = inputElement.checked;
+  });
+
+  listElement.append(taskElement);
+}
