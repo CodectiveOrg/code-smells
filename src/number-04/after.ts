6d0e455ccd4e0edf780a81e6b977fc0ad840d385
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-29 11:47:55 +0330

    [ADD] number 04

--- src/number-04/after.ts
+++ src/number-04/after.ts
@@ -1,1 +1,21 @@
-// Code Smell: Name | After
+// Code Smell: Data Clumps | After
+
+export class Graph {
+  public drawPoint(point: Point): void {
+    // ...
+  }
+
+  public drawCircle(centerPoint: Point, radius: number): void {
+    // ...
+  }
+
+  public drawLine(startPoint: Point, endPoint: Point): void {
+    // ...
+  }
+}
+
+interface Point {
+  x: number;
+  y: number;
+  z: number;
+}
--- src/number-04/before.ts
+++ src/number-04/before.ts
@@ -1,1 +1,27 @@
-// Code Smell: Name | Before
+// Code Smell: Data Clumps | Before
+
+export class Graph {
+  public drawPoint(x: number, y: number, z: number): void {
+    // ...
+  }
+
+  public drawCircle(
+    centerX: number,
+    centerY: number,
+    centerZ: number,
+    radius: number,
+  ): void {
+    // ...
+  }
+
+  public drawLine(
+    startX: number,
+    startY: number,
+    startZ: number,
+    endX: number,
+    endY: number,
+    endZ: number,
+  ): void {
+    // ...
+  }
+}
