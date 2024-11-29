bdffc8620f8773ab5debdeee2b7913a9ad3cbffc
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-29 11:41:45 +0330

    [ADD] number 03

--- src/number-03/after.ts
+++ src/number-03/after.ts
@@ -1,1 +1,12 @@
-// Code Smell: Name | After
+// Code Smell: Comments | After
+
+const DISCOUNT_THRESHOLD = 1000;
+const DISCOUNT = 0.1;
+
+export function applyDiscountIfPossible(price: number): number {
+  if (price > DISCOUNT_THRESHOLD) {
+    return price * (1 - DISCOUNT);
+  }
+
+  return price;
+}
--- src/number-03/before.ts
+++ src/number-03/before.ts
@@ -1,1 +1,11 @@
-// Code Smell: Name | Before
+// Code Smell: Comments | Before
+
+export function applyDiscountIfPossible(price: number): number {
+  // We only add discounts if price is above 1000
+  if (price > 1000) {
+    // Discount is 10%, so the final value has to be multiplied by 0.9
+    return price * 0.9;
+  }
+  // Otherwise just return price as is
+  return price;
+}
