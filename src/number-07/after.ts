469a36af48c74dfb83a3473f2d92d0fa976fb82a
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-29 12:29:17 +0330

    [ADD] number 07

--- src/number-07/after.ts
+++ src/number-07/after.ts
@@ -1,1 +1,16 @@
-// Code Smell: Name | After
+// Code Smell: Dead Code | Before
+
+import { TaskModel } from "../demo-purpose/models/task.model";
+
+class Api {
+  public async fetchTasks(): Promise<TaskModel[] | null> {
+    const response = await fetch(`${process.env.API_BASE_URL}/tasks`);
+
+    if (!response.ok) {
+      console.error("Cannot fetch tasks.");
+      return null;
+    }
+
+    return await response.json();
+  }
+}
--- src/number-07/before.ts
+++ src/number-07/before.ts
@@ -1,1 +1,18 @@
-// Code Smell: Name | Before
+// Code Smell: Dead Code | Before
+
+import { TaskModel } from "../demo-purpose/models/task.model";
+
+class Api {
+  private readonly BASE_URL = "https://api.com";
+
+  public async fetchTasks(): Promise<TaskModel[] | null> {
+    const response = await fetch(`${process.env.API_BASE_URL}/tasks`);
+
+    if (!response.ok) {
+      console.error("Cannot fetch tasks.");
+      return null;
+    }
+
+    return await response.json();
+  }
+}
