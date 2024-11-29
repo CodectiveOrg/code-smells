8b6122ed477a569fbe463f30cfc9249b29c586d3
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-29 12:39:37 +0330

    [ADD] number 08

--- src/number-08/after.ts
+++ src/number-08/after.ts
@@ -1,1 +1,38 @@
-// Code Smell: Name | After
+// Code Smell: Divergent Change | After
+
+export class Toaster {
+  public static SUCCESS: Variant = {
+    label: "Success",
+    datasetType: "success",
+    logMethod: console.info,
+  };
+
+  public static WARNING: Variant = {
+    label: "Warning",
+    datasetType: "warning",
+    logMethod: console.warn,
+  };
+
+  public static ERROR: Variant = {
+    label: "Error",
+    datasetType: "error",
+    logMethod: console.error,
+  };
+
+  private get toastElement(): HTMLElement {
+    return document.querySelector("#toast-container")!;
+  }
+
+  public show(message: string, variant: Variant): void {
+    this.toastElement.innerHTML = `${variant.label}: ${message};`;
+    this.toastElement.dataset.type = variant.datasetType;
+
+    variant.logMethod(`[TOAST] ${message}`);
+  }
+}
+
+interface Variant {
+  label: string;
+  datasetType: string;
+  logMethod: (message: string) => unknown;
+}
--- src/number-08/before.ts
+++ src/number-08/before.ts
@@ -1,1 +1,28 @@
-// Code Smell: Name | Before
+// Code Smell: Divergent Change | Before
+
+export class Toaster {
+  private get toastElement(): HTMLElement {
+    return document.querySelector("#toast-container")!;
+  }
+
+  public showSuccess(message: string): void {
+    this.toastElement.innerHTML = `Success: ${message};`;
+    this.toastElement.dataset.type = "success";
+
+    console.info(`[TOAST] ${message}`);
+  }
+
+  public showWarning(message: string): void {
+    this.toastElement.innerHTML = `Warning: ${message};`;
+    this.toastElement.dataset.type = "warning";
+
+    console.warn(`[TOAST] ${message}`);
+  }
+
+  public showError(message: string): void {
+    this.toastElement.innerHTML = `Error: ${message};`;
+    this.toastElement.dataset.type = "error";
+
+    console.error(`[TOAST] ${message}`);
+  }
+}
