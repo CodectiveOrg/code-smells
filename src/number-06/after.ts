0d821900bebd0c1220178d2d95ebcefe54bce77a
BijanProgrammer <bijaneisapour@gmail.com>
2024-11-29 12:23:56 +0330

    [ADD] number 06

--- src/number-06/after.ts
+++ src/number-06/after.ts
@@ -1,1 +1,20 @@
-// Code Smell: Name | After
+// Code Smell: Primitive Obsession | After
+
+export interface User {
+  username: string;
+  fullName: FullName;
+  address: Address;
+}
+
+interface FullName {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+}
+
+interface Address {
+  province: string;
+  city: string;
+  street: string;
+  zipCode: string;
+}
--- src/number-06/before.ts
+++ src/number-06/before.ts
@@ -1,1 +1,12 @@
-// Code Smell: Name | Before
+// Code Smell: Primitive Obsession | Before
+
+export interface User {
+  username: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  province: string;
+  city: string;
+  street: string;
+  zipCode: string;
+}
